025bd590b9db06711b51536fd29c50e26ec97874
Pedro Félix <pedrofelixalves@gmail.com>
2024-03-01 15:57:56 +0000

    Create rt_openephys_test1.py

---
+++ rt_openephys_test1.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 19:44:34 2024
+
+@author: pedro
+"""
+
+import pyNN.spiNNaker as sim
+import pyNN.utility.plotting as plot
+import matplotlib.pyplot as plt 
+import sys
+import time
+import threading
+from open_ephys.streaming import EventListener
+
+
+
+original_stdout = sys.stdout
+live_input = True
+address='127.0.0.1:5557'
+stream = EventListener(address)
+label="sender"
+connection= sim.external_devices.SpynnakerLiveSpikesConnection(send_labels=["sender"], local_port=19995)
+
+
+# === Simulation Setup ===
+sim.setup(timestep=1.0)
+sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 100)
+
+if live_input:
+    input_pop = sim.Population(1, sim.external_devices.SpikeInjector(database_notify_port_num=connection.local_port), label="sender")
+else:
+    input_pop = sim.Population(1, sim.SpikeSourceArray(spike_times=[0, 100, 200, 300, 400]), label="sender")
+    
+main_pop = sim.Population(1, sim.IF_curr_exp(), label="receiver")
+input_proj = sim.Projection(input_pop, main_pop, sim.OneToOneConnector(), synapse_type=sim.StaticSynapse(weight=5, delay=1))
+
+# === External Sender Setup ===
+
+def spike_callback(info):
+    print(info)
+    connection.send_spike(label,0,send_full_keys=True)
+    
+def start(label):
+    if live_input:
+        stream.start(spike_callback=spike_callback)
+    
+    
+def input_thread(label,sender):
+    time_between_spikes = 0.1
+    quit = 0
+    input_spike_duration = 40 # secs
+    print("Start sending input spikes for {} seconds...".format(input_spike_duration))
+    while(quit < input_spike_duration):
+        time.sleep(time_between_spikes)
+        sender.send_spike(label, 0, send_full_keys=True)
+        quit += time_between_spikes
+    print("Input Spikes stopped")
+
+
+
+connection.add_start_resume_callback(label,start)
+
+
+
+    
+
+# === External Receiver Setup ===
+sim.external_devices.activate_live_output_for(main_pop, database_notify_port_num=19994)
+
+def receive_spikes(label, time, neuron_ids):
+    with open("output.txt", "a") as f:
+        sys.stdout = f
+        for neuron_id in neuron_ids:
+                print("Received spike at time {} from {} {}".format(time, label, neuron_id))
+    sys.stdout = original_stdout
+live_spikes_connection = sim.external_devices.SpynnakerLiveSpikesConnection(receive_labels=["receiver"], local_port=19996)
+live_spikes_connection.add_receive_callback("receiver", receive_spikes)
+
+
+main_pop.record(["spikes"])
+
+# === Start the Simulation ===
+simtime = 50000
+sim.run(simtime)
+
+# === Retrieve the Results (Post Simulation) ===
+neo = main_pop.get_data(variables=["spikes"])
+spikes = neo.segments[0].spiketrains
+sim.end()
+
+# === Plot the Results of the Simulation ===
+plot.Figure(
+    plot.Panel(spikes, yticks=True, xticks=True, markersize=5, xlim=(0, simtime)),
+    title="Tutorial External Devices"
+)
+plt.xlabel("Simulation Time [ms]")
+plt.locator_params(axis="y", integer=True, tight=True)
+plt.show()
