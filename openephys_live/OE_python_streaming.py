513db8ba28d9ade7b8eb1e331007b6357b9b495d
Pedro Félix <pedrofelixalves@gmail.com>
2024-03-04 18:01:53 +0000

    Update OE_python_streaming.py

--- openephys_live/OE_python_streaming.py
+++ openephys_live/OE_python_streaming.py
@@ -11,8 +11,15 @@ stream = EventListener()
 def ttl_callback(info):
     if info['line'] == 2 and info['state']:
         print('Rising event on line 2')
-        
+
+count=0
 def spike_callback(info):
-  print(info)      
-        
+  print(info)
+  spike_callback.counter+=1
+
+spike_callback.counter=0
+  
+
 stream.start(ttl_callback=ttl_callback, spike_callback=spike_callback)
+
+print(spike_callback.counter)
